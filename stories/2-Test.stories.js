58a82ba4aa4d6792e8db83261358ddc252319d4b
lyhd <lyhong.rupp@gmail.com>
2020-01-18 19:15:15 +1100

    installed and configured storybook knobs addon

--- stories/2-Test.stories.js
+++ stories/2-Test.stories.js
@@ -1,12 +1,33 @@
 import React from "react";
 import { action } from "@storybook/addon-actions";
+import { storiesOf } from "@storybook/react";
+import { withKnobs, text, boolean, color } from "@storybook/addon-knobs/react";
 
-export default {
-  title: "Test"
-};
+// export default {
+//   title: "Test"
+// };
 
-export const test = () => (
-  <textarea onClick={action("clicked")}>
-    Hong test textarea in storybook
-  </textarea>
-);
+// export const test = () => (
+//   <textarea onClick={action("clicked")}>
+//     Hong test textarea in storybook
+//   </textarea>
+// );
+
+storiesOf("Storybook Knobs", module)
+  .addDecorator(withKnobs)
+
+  .add("with a button", () => (
+    <button
+      disabled={boolean("Disabled", false)}
+      style={{ backgroundColor: color("Color", "red") }}
+    >
+      {text("Label", "Hi guys")}
+    </button>
+  ))
+
+  .add("as dynamic variables", () => {
+    const name = text("Name", "Hong");
+    const content = `Hi I'm ${name}. Nice to meet you!`;
+
+    return <div>{content}</div>;
+  });
